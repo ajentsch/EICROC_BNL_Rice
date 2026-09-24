45276fbcb639815be83c8003176e7375cde22932
Tonko Ljubicic <tonko.ljubicic@gmail.com>
2026-09-24 06:27:12 -0400

    Starting AXAU15 and RBv1 readout

---
+++ src/AXAU/axau.C
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#include <LOG/rtsLog.h>
+volatile int rtsLogLevel ;
+
+
+int main(int argc, char *argv[])
+{
+	rtsLogLevel = 0 ;
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0 ;
+}
